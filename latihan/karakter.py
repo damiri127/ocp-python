e75bfe86d543666053d90fca7fb73860a966d5b9
anantapk03 <anantapk03@gmail.com>
2022-05-23 21:38:55 +0700

    ananta version, karakter.py

---
+++ latihan/karakter.py
@@ -0,0 +1,17 @@
+from abc import ABC , abstractmethod 
+
+class Karakkter :
+    def __init__(self, nama: str, power: int):
+        self.__nama = nama
+        self.__power = power 
+    
+    def get_nama (self) -> str :
+        return self.__nama 
+    
+    def get_power (self) -> int: 
+        return self.__power
+    
+    
+    @abstractmethod
+    def menyerang (type : str) -> str :
+        pass 
