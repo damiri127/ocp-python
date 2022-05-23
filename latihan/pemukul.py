2e52681b627eb3d324600e32fff93f8782d0e115
damiri127 <damiricepri127@gmail.com>
2022-05-23 22:01:59 +0700

    penembak + pemukul by ceps

---
+++ latihan/pemukul.py
@@ -0,0 +1,9 @@
+from karakter import Karakter
+
+class Pemukul(Karakter):
+    
+    def __init__(self, nama : str, power: int):
+        super().__init__(nama, power)
+    
+    def menyerang(self):
+        return self.get_power()
